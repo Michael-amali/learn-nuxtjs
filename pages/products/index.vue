94d902f2faab9d9b4849d9c601f0dd414f80df88
Michael Acheampong <michael.acheampong@amalitech.com>
2022-05-19 13:47:40 +0000

    Implement asyncData, head, and axios fetching

--- pages/products/index.vue
+++ pages/products/index.vue
@@ -2,12 +2,12 @@
   <div class="container">
     <LargeCardDisplay
       v-for="cardInfo in largeCardInfo.slice(0,1)"
-      :key="cardInfo.id"
+      :key="cardInfo.id + Math.random()"
       :cards-section="cardInfo"
     />
     <SmallCardDisplay
       v-for="cardInfo in smallCardSections"
-      :key="cardInfo.id"
+      :key="cardInfo.id + Math.random()"
       :cards-section="cardInfo"
     />
   </div>
@@ -17,10 +17,33 @@
 import { largeCardSections, smallCardSections } from '@/assets/data.js'
 export default {
   name: 'ProductPage',
+  asyncData () {
+    return {
+      newTitle: 'testing asyncData'
+    }
+  },
   data () {
     return {
       largeCardInfo: largeCardSections,
       smallCardSections
+    }
+  },
+  head () {
+    return {
+      title: 'Products',
+      script: [{ src: '/products/index.js' }],
+      meta: [{ hid: 'description', name: 'description', content: 'Fire extinguisher for rent' }]
+    }
+  },
+  mounted () {
+    this.getUsers()
+    console.log(process.env.TEST_ENV, 'env')
+  },
+  methods: {
+    getUsers () {
+      this.$axios.$get('https://reqres.in/api/users?page=2').then((res) => {
+        console.log(res.data)
+      })
     }
   }
 }
