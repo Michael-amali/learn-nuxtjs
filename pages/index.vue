77c5039c864e4ecac97bf301690a8e1c2bf38832
Michael Acheampong <michael.acheampong@amalitech.com>
2022-05-19 07:42:06 +0000

    initial commit

---
+++ pages/index.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <Hero />
+    <div class="wrapper">
+      <LargeCardDisplay
+        v-for="cardInfo in largeCardInfo"
+        :key="cardInfo.id"
+        :cards-section="cardInfo"
+      />
+      <SmallCardDisplay
+        v-for="cardInfo in smallCardSections"
+        :key="cardInfo.id"
+        :cards-section="cardInfo"
+      />
+    </div>
+  </div>
+</template>
+
+<script>
+import { largeCardSections, smallCardSections } from '@/assets/data.js'
+export default {
+  name: 'IndexPage',
+  data () {
+    return {
+      largeCardInfo: largeCardSections,
+      smallCardSections
+    }
+  }
+}
+</script>
+
+<style>
+.wrapper {
+  margin-top: 17rem;
+}
+
+</style>
