df4468ec8ea7918239e973d4853e1d5938591a36
Michael Acheampong <michael.acheampong@amalitech.com>
2022-06-02 10:15:18 +0000

    Updage package.json with tailwind to generate tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+module.exports = {
+  content: [
+    './components/**/*.{js,vue,ts}',
+    './layouts/**/*.vue',
+    './pages/**/*.vue',
+    './plugins/**/*.{js,ts}',
+    './nuxt.config.{js,ts}'
+  ],
+  theme: {
+    screens: {
+      xs: '540px',
+      ...defaultTheme.screens
+    },
+    extend: {}
+  },
+  plugins: []
+}
